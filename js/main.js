12a5bd1f71b82163acc88b629ee5a5461749f807
matttt <matthew.martori@gmail.com>
2018-01-29 22:52:48 -0500

    removed .DS_store

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ const w = window.innerWidth - 20
 const h = window.innerHeight - 20
 
 function pointAtAngle(a, r, x, y) {
-    let nx = Math.sin(a) * r + x
-    let ny = Math.cos(a) * r + y
+  let nx = Math.sin(a) * r + x
+  let ny = Math.cos(a) * r + y
 
-    return [nx, ny]
+  return [nx, ny]
 }
 
 let sun, earth, moon
@@ -14,50 +14,49 @@ let stars = []
 const halfEarthCycle = 50 * Math.PI
 
 function setup() {
-    createCanvas(w, h)
+  createCanvas(w, h)
 
-    noStroke()
+  noStroke()
 
-    for (var i = 0; i < 300; i++) {
-        let rx = Math.random() * w
-        let ry = Math.random() * h
-        let r = Math.random() * 3
-        stars.push(new Star(rx, ry, r, i))
-    }
+  for (var i = 0; i < 300; i++) {
+    let rx = Math.random() * w
+    let ry = Math.random() * h
+    let r = Math.random() * 3
+    stars.push(new Star(rx, ry, r, i))
+  }
 
 
-    sun = new Sun(w / 2, h / 2)
-    earth = new Earth(sun)
-    moon = new Moon(earth)
+  sun = new Sun(w / 2, h / 2)
+  earth = new Earth(sun)
+  moon = new Moon(earth)
 }
 
 function keyPressed() {
-    sun.setTarget(Math.random()*w,Math.random()*h)
+  sun.setTarget(Math.random() * w, Math.random() * h)
 }
 
-
 function draw() {
-    background(77, 80, 97)
+  background(77, 80, 97)
 
-    const frameToRad = map(frameCount, 0, 700, 0, 2 * Math.PI);
-    const prog = frameToRad % 2 * Math.PI
+  const frameToRad = map(frameCount, 0, 700, 0, 2 * Math.PI);
+  const prog = frameToRad % 2 * Math.PI
 
-    for (let star of stars) {
-        star.update()
-        star.draw()
-    }
+  for (let star of stars) {
+    star.update()
+    star.draw()
+  }
 
-    sun.update()
+  sun.update()
 
-    moon.update(prog)
-    earth.update(prog)
-    moon.draw()    
+  moon.update(prog)
+  earth.update(prog)
+  moon.draw()
+  earth.draw()
+
+  sun.draw()
+
+  if (prog > Math.PI) {
     earth.draw()
-
-    sun.draw()
-
-    if (prog > Math.PI) {
-        earth.draw()
-        moon.draw()
-    }
+    moon.draw()
+  }
 }
